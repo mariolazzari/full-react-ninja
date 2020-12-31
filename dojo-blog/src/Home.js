4a3404570d3c1041f2026a9d628ee3bbfd1e5cab
Mario Lazzari <mario.lazzari@gmail.com>
2020-12-31 18:36:40 +0100

    Done lesson 10

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,9 +1,21 @@
-import React from "react";
+import { useState } from "react";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "blog1", body: "body1", author: "mario", id: 1 },
+    { title: "blog2", body: "body2", author: "mario", id: 3 },
+    { title: "blog3", body: "body3", author: "mario", id: 3 },
+    { title: "blog4", body: "body4", author: "mario", id: 4 },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map(blog => (
+        <div className="blogPreview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
