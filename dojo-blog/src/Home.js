ac32d9562ccd72080706e5bf87fef30c76eab4b6
Mario Lazzari <mario.lazzari@gmail.com>
2021-01-08 19:18:17 +0100

    Done lesson 17

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,29 +2,23 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "blog1", body: "body1", author: "mario", id: 1 },
-    { title: "blog2", body: "body2", author: "yoshi", id: 2 },
-    { title: "blog3", body: "body3", author: "luigi", id: 3 },
-    { title: "blog4", body: "body4", author: "mario", id: 4 },
-  ]);
+  const [blogs, setBlogs] = useState();
   const [name, setName] = useState("mario");
-
-  const handleDelete = id => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const handleDelete = id => {};
 
   useEffect(() => {
-    console.log("useEffect runs");
-    console.log(name);
+    fetch("http://localhost:8000/blogs")
+      .then(res => res.json())
+      .then(data => setBlogs(data));
   }, [name]);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName("Luigi")}>Change name</button>
-      <p>{name}</p>
+      {blogs ? (
+        <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      ) : (
+        <p>Loading blogs...</p>
+      )}
     </div>
   );
 };
