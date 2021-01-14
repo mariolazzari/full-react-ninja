e6d61825dda26fde30eac58cb26ff2b017daaffd
Mario Lazzari <mario.lazzari@gmail.com>
2021-01-14 19:11:42 +0100

    Done lesson 22

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/create" exact component={Create} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
